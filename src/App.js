b88bfe867d033d3c2801b82dd28061e6b3c89783
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-11-28 15:33:01 -0700

    Added Lots of CSS and deleted unecessary logo files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import axios from "axios";
+import styles from "./App.module.css";
+import check from "./accepted.png";
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -91,15 +93,15 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
-      {stories.isError && <p>Somthing went wrong...</p>}
+
+      {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
@@ -127,13 +129,16 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className={styles.label}>
+        {children}
+      </label>
       <input
         id={id}
         type={type}
         value={value}
         autoFocus={isFocused}
         onChange={onInputChange}
+        className={styles.input}
       />
     </>
   );
@@ -152,16 +157,20 @@ const Item = ({ item, onRemoveItem }) => {
     onRemoveItem(item);
   };
   return (
-    <li>
-      <span>
+    <li className={styles.item}>
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title} </a>
       </span>
-      <span>Author: {item.author} </span>
-      <span>Comments: {item.num_comments}, </span>
-      <span>Points: {item.points} </span>
-      <span>
-        <button type="button" onClick={handleRemoveItem}>
-          Dismiss
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={handleRemoveItem}
+          className={`${styles.button} ${styles.buttonSmall}`}
+        >
+          <img src={check} className={styles.checkMark} alt="checkmark" />
         </button>
       </span>
     </li>
@@ -169,7 +178,7 @@ const Item = ({ item, onRemoveItem }) => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className={styles.searchForm}>
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -178,7 +187,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search: </strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className={`${styles.button} ${styles.buttonLarge}`}
+    >
       Submit
     </button>
   </form>
