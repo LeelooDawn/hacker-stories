cd19242df1955792588968d6e4f5c54729b950ef
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2023-12-12 14:55:24 -0700

    Trying Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("something truthy and falsy", () => {
+  test("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  test("false to be false", () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe("App component", () => {
+  test("removes an item when clicking the Dismiss button", () => {});
+
+  test("requests some initial stories from an API", () => {});
 });
